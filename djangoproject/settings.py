f860f9222b32bcbd726fce9f1bfb9ef2742dee74
unknown <skhaleejal@gmail.com>
2025-05-21 16:48:30 +0400

    change

--- djangoproject/settings.py
+++ djangoproject/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'invoicemgmt',
+    'invoicemgmt','testapp',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
