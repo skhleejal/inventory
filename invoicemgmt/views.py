8a72a5bfab6e86ac53e2e9644d03439a4be1285a
unknown <skhaleejal@gmail.com>
2025-05-23 18:14:44 +0400

    test urls

--- invoicemgmt/views.py
+++ invoicemgmt/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Customer, Invoice
 
-# Create your views here.
+def home(request):
+    return render(request, 'invoicemgmt/home.html')
+
+def customer_list(request):
+    customers = Customer.objects.all()
+    return render(request, 'invoicemgmt/customer_list.html', {'customers': customers})
+
+def invoice_list(request):
+    invoices = Invoice.objects.all()
+    return render(request, 'invoicemgmt/invoice_list.html', {'invoices': invoices})
